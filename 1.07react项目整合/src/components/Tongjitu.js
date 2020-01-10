76e0b4167e475df51b01ca245cca9f5b9b8b3388
wangaobin1314520 <3362196597@qq.com>
2020-01-10 16:49:43 +0800

    add: react-echarts

--- 1.07react项目整合/src/components/Tongjitu.js
+++ 1.07react项目整合/src/components/Tongjitu.js
@@ -17,13 +17,13 @@ class EchartsTest extends Component {
             title: { text: 'ECharts 入门示例' },
             tooltip: {},
             xAxis: {
-                data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "内裤", "袜子"]
             },
             yAxis: {},
             series: [{
                 name: '销量',
                 type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
+                data: [10, 20, 36, 10, 10, 20]
             }]
         });
     }
